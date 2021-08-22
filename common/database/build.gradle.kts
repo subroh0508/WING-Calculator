b132e998125d62de60859a0f86bebead15e317c9
subroh0508 <subroh.0508@gmail.com>
2021-08-23 03:17:29 +0900

    [edit]Enable sqldelight plugin

--- common/database/build.gradle.kts
+++ common/database/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("multiplatform")
     `android-multiplatform`
+    id("com.squareup.sqldelight")
 }
 
 kotlin {
@@ -9,15 +10,30 @@ kotlin {
     // ios()
 
     sourceSets {
+        named("commonTest") {
+            dependencies {
+                implementation(kotlinTestCommon)
+            }
+        }
         named("androidMain") {
             dependencies {
                 implementation(Libraries.SQLDelight.jvm)
                 implementation(Libraries.SQLDelight.android)
             }
         }
+        named("androidTest") {
+            dependencies {
+                implementation(kotlinTestJUnit)
+            }
+        }
         named("desktopMain") {
             dependencies {
                 implementation(Libraries.SQLDelight.jvm)
+            }
+        }
+        named("desktopTest") {
+            dependencies {
+                implementation(kotlinTestJUnit)
             }
         }
         /*
