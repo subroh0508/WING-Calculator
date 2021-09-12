fc8f3c08ca4c5863f01703290ee1cdd9aeb5dd99
subroh0508 <subroh.0508@gmail.com>
2021-09-12 23:58:19 +0900

    [edit]Remove suspend modifier from database methods

--- common/database/src/commonMain/kotlin/net/subroh0508/wingcalculator/database/ProducerDatabase.kt
+++ common/database/src/commonMain/kotlin/net/subroh0508/wingcalculator/database/ProducerDatabase.kt
@@ -1,22 +1,18 @@
 package net.subroh0508.wingcalculator.database
 
-import com.squareup.sqldelight.runtime.coroutines.asFlow
-import com.squareup.sqldelight.runtime.coroutines.mapToOne
-import com.squareup.sqldelight.runtime.coroutines.mapToOneOrNull
-import kotlinx.coroutines.flow.first
 import kotlinx.datetime.Clock
 
 class ProducerDatabase(
     private val queries: ProducerQueries,
 ) {
-    suspend fun getCurrent(): Producer {
-        val anonymous = queries.selectAnonymous().asFlow().mapToOneOrNull().first()
+    fun getCurrent(): Producer {
+        val anonymous = queries.selectAnonymous().executeAsOneOrNull()
 
         if (anonymous != null) {
             return anonymous
         }
 
         queries.addAnonymous(Clock.System.now().toEpochMilliseconds())
-        return queries.selectAnonymous().asFlow().mapToOne().first()
+        return queries.selectAnonymous().executeAsOne()
     }
 }
