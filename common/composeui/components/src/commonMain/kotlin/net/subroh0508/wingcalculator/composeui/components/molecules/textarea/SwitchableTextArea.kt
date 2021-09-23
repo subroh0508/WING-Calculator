352da3675eeba1f58bbac089857d252afb1398d9
subroh0508 <subroh.0508@gmail.com>
2021-09-23 20:24:23 +0900

    [edit]Change SwitchableTextArea UI

--- common/composeui/components/src/commonMain/kotlin/net/subroh0508/wingcalculator/composeui/components/molecules/textarea/SwitchableTextArea.kt
+++ common/composeui/components/src/commonMain/kotlin/net/subroh0508/wingcalculator/composeui/components/molecules/textarea/SwitchableTextArea.kt
@@ -5,9 +5,8 @@ package net.subroh0508.wingcalculator.composeui.components.molecules.textarea
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Save
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,28 +22,6 @@ fun SwitchableTextArea(
     var editable by remember { mutableStateOf(false) }
     var inputText by remember(text) { mutableStateOf(text) }
 
-    if (editable) {
-        OutlinedTextField(
-            inputText ?: "",
-            { s ->
-                inputText = s.takeIf(String::isNotBlank)
-                onTextChanged(s.takeIf(String::isNotBlank))
-            },
-            label = { Text("メモ") },
-            placeholder = { Text("メモを入力") },
-            trailingIcon = {
-                IconButton(
-                    onClick = {
-                        editable = false
-                        onSaveClick(inputText?.takeIf(String::isNotBlank))
-                    }
-                ) { Icon(Icons.Default.Save, contentDescription = "Save") }
-            },
-            modifier = modifier,
-        )
-        return
-    }
-
     Card(
         elevation = 0.dp,
         modifier = modifier.border(
@@ -54,24 +31,86 @@ fun SwitchableTextArea(
         ),
     ) {
         Column {
-            Row(Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 8.dp)) {
-                Text(
-                    "メモ",
-                    style = MaterialTheme.typography.caption,
-                    modifier = Modifier.weight(1F),
-                )
-            }
-
             Text(
-                text ?: "",
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.padding(horizontal = 16.dp),
+                "メモ",
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 8.dp),
             )
-            TextButton(
-                onClick = { editable = true },
-                modifier = Modifier.padding(8.dp)
-                    .align(Alignment.End),
-            ) { Text("編集") }
+            TextAreaContent(
+                editable,
+                inputText,
+                modifier = Modifier.padding(horizontal = 16.dp),
+            ) { s ->
+                inputText = s
+                onTextChanged(s)
+            }
+            TextAreaButtons(
+                editable,
+                onPositiveClick = { b ->
+                    if (!b) {
+                        onSaveClick(inputText?.takeIf(String::isNotBlank))
+                    }
+
+                    editable = b
+                },
+                onNegativeClick = {
+                    inputText = text
+                    editable = false
+                },
+                modifier = Modifier.padding(8.dp),
+            )
         }
     }
+}
+
+@Composable
+private fun TextAreaContent(
+    editable: Boolean,
+    text: String?,
+    modifier: Modifier = Modifier,
+    onTextChanged: (String?) -> Unit,
+) {
+    if (!editable) {
+        Text(
+            text ?: "",
+            style = MaterialTheme.typography.body1,
+            modifier = modifier,
+        )
+        return
+    }
+
+    Box(modifier) {
+        if (text == null) {
+            Text(
+                "メモを入力",
+                style = MaterialTheme.typography.button,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.6F),
+            )
+        }
+
+        BasicTextField(
+            text ?: "",
+            onValueChange = { onTextChanged(it.takeIf(String::isNotBlank)) },
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
+
+@Composable
+private fun ColumnScope.TextAreaButtons(
+    editable: Boolean,
+    onPositiveClick: (Boolean) -> Unit,
+    onNegativeClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) = Row(modifier.align(Alignment.End)) {
+    if (editable) {
+        TextButton(
+            onClick = onNegativeClick,
+            modifier = Modifier.padding(end = 8.dp),
+        ) { Text("キャンセル") }
+    }
+    TextButton(
+        onClick = { onPositiveClick(!editable) },
+    ) { Text(if (editable) "保存" else "編集") }
 }
