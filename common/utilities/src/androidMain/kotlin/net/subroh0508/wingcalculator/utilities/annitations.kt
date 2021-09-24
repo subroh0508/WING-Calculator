fe9d5f698ce1c4269909771f49747ad2c2f8d88a
subroh0508 <subroh.0508@gmail.com>
2021-09-24 21:46:43 +0900

    [add]Add annotations.kt to utilities

---
+++ common/utilities/src/androidMain/kotlin/net/subroh0508/wingcalculator/utilities/annitations.kt
@@ -0,0 +1,3 @@
+package net.subroh0508.wingcalculator.utilities
+
+actual typealias JvmInline = kotlin.jvm.JvmInline
---
+++ common/utilities/src/jsMain/kotlin/net/subroh0508/wingcalculator/utilities/annitations.kt
@@ -0,0 +1,3 @@
+package net.subroh0508.wingcalculator.utilities
+
+actual annotation class JvmInline
