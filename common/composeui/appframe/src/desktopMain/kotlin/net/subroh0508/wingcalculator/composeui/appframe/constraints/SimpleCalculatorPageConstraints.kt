e2ff0881402086904301721c4bffe9cfe6c08bcf
subroh0508 <subroh.0508@gmail.com>
2021-09-25 15:40:52 +0900

    [edit]Change when to find in SimpleCalculatorPageConstraints invoke method

--- common/composeui/appframe/src/desktopMain/kotlin/net/subroh0508/wingcalculator/composeui/appframe/constraints/SimpleCalculatorPageConstraints.kt
+++ common/composeui/appframe/src/desktopMain/kotlin/net/subroh0508/wingcalculator/composeui/appframe/constraints/SimpleCalculatorPageConstraints.kt
@@ -4,9 +4,7 @@ package net.subroh0508.wingcalculator.composeui.appframe.constraints
 
 import androidx.compose.ui.unit.Dp
 
-actual operator fun SimpleCalculatorPageConstraints.Companion.invoke(maxWidth: Dp) = when (maxWidth) {
-    in SimpleCalculatorPageConstraints.ONE_PANEL_MODAL -> SimpleCalculatorPageConstraints.ONE_PANEL_MODAL
-    in SimpleCalculatorPageConstraints.ONE_PANEL_SHRINKABLE_MODAL -> SimpleCalculatorPageConstraints.ONE_PANEL_SHRINKABLE_MODAL
-    in SimpleCalculatorPageConstraints.TWO_PANELS_SHRINKABLE_MODAL -> SimpleCalculatorPageConstraints.TWO_PANELS_SHRINKABLE_MODAL
-    else -> SimpleCalculatorPageConstraints.TWO_PANELS_SHRINKABLE_PERSIST
-}
+actual operator fun SimpleCalculatorPageConstraints.Companion.invoke(maxWidth: Dp) =
+    SimpleCalculatorPageConstraints.values().find {
+        maxWidth in it
+    } ?: SimpleCalculatorPageConstraints.ONE_PANEL_MODAL
