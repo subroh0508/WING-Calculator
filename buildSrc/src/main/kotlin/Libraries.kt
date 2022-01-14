d80863a8e8eca7f3f3050b42526d05da76663942
subroh0508 <subroh.0508@gmail.com>
2022-01-15 00:09:04 +0900

    [update]Update Kotlin -> 1.6.10 / compose-jb -> 1.0.1

--- buildSrc/src/main/kotlin/Libraries.kt
+++ buildSrc/src/main/kotlin/Libraries.kt
@@ -2,28 +2,28 @@
 
 object Libraries {
     object Coroutines {
-        private const val version = "1.5.1"
+        private const val version = "1.6.0"
         const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
     }
 
     object Datetime {
-        private const val version = "0.3.0"
+        private const val version = "0.3.2"
         const val common = "org.jetbrains.kotlinx:kotlinx-datetime:$version"
     }
 
     object Jetpack {
-        private const val appCompatVersion = "1.3.0"
+        private const val appCompatVersion = "1.4.1"
         const val appCompat = "androidx.appcompat:appcompat:$appCompatVersion"
 
-        private const val coreVersion = "1.6.0"
+        private const val coreVersion = "1.7.0"
         const val core = "androidx.core:core-ktx:$coreVersion"
 
-        private const val activityVersion = "1.3.0"
+        private const val activityVersion = "1.4.0"
         const val activityCompose = "androidx.activity:activity-compose:$activityVersion"
     }
 
     object SQLDelight {
-        private const val version = "1.5.2"
+        private const val version = "1.5.3"
         const val plugin = "com.squareup.sqldelight:gradle-plugin:$version"
 
         const val jvm = "com.squareup.sqldelight:sqlite-driver:$version"
@@ -34,7 +34,7 @@ object Libraries {
     }
 
     object Koin {
-        private const val version = "3.1.2"
+        private const val version = "3.1.5"
 
         const val core = "io.insert-koin:koin-core:$version"
         const val android = "io.insert-koin:koin-android:$version"
